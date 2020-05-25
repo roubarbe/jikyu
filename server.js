d7c30192c12aa2865bd9efd4a15f6d63463d071c
Xavier <provost.pascal@gmail.com>
2020-05-25 02:14:47 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("Loading Express...");
+let express = require("express");
+
+console.log("Initializing Express...");
+let app = express();
+
+app.use(express.static('static'));
+
+app.listen(process.env.PORT || 21134, function () {
+	console.log('Jikyu running on port', process.env.PORT || 21134);
+});
